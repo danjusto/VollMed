e764730fee15b82116628d2d9ce36f08604a920c
Daniel Justo <justodaniel29@gmail.com>
2023-05-22 16:31:28 -0300

    autenticacao e autorizacao com JWT

--- src/main/java/med/voll/api/infra/exception/TratadorDeError.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeError.java
@@ -1,7 +1,10 @@
-package med.voll.api.infra;
+package med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +21,21 @@ public class TratadorDeError {
     public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
         var erros = ex.getFieldErrors();
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +ex.getLocalizedMessage());
     }
 
     private record DadosErroValidacao(String campo, String mensagem) {
---
+++ src/main/resources/db/migration/V5__create-table-usuarios.sql
@@ -0,0 +1,6 @@
+CREATE TABLE usuarios(
+    id bigint not null auto_increment,
+    login varchar(100) not null,
+    senha varchar(255) not null,
+    primary key(id)
+);
